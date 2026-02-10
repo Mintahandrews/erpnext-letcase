bdf2264e5fafbc66ce6e0df97612a603fc026389
codemintah <86488838+Mintahandrews@users.noreply.github.com>
2026-02-10 15:53:01 +0000

    feat(railway): add health check and dynamic port configuration

--- railway/railway-cmd.sh
+++ railway/railway-cmd.sh
@@ -5,7 +5,7 @@ echo "-> Clearing cache"
 su frappe -c "bench execute frappe.cache_manager.clear_global_cache"
 
 echo "-> Bursting env into config"
-envsubst '$RFP_DOMAIN_NAME' < /home/$systemUser/temp_nginx.conf > /etc/nginx/conf.d/default.conf
+envsubst '$RFP_DOMAIN_NAME,$PORT' < /home/$systemUser/temp_nginx.conf > /etc/nginx/conf.d/default.conf
 envsubst '$PATH,$HOME,$NVM_DIR,$NODE_VERSION' < /home/$systemUser/temp_supervisor.conf > /home/$systemUser/supervisor.conf
 
 echo "-> Starting nginx"
