3952204b25fd63fc842655f936c7758a688b4ce2
codemintah <86488838+Mintahandrews@users.noreply.github.com>
2026-02-10 21:04:29 +0000

    fix(railway): run setup commands as frappe user and improve setup instructions

--- railway/railway-setup.sh
+++ railway/railway-setup.sh
@@ -4,14 +4,17 @@ set -e
 # -> Run entrypoint
 # somehow when specify custom cmd in railway,
 # it doesn't run entrypoint first, so we need to run it here.
-sudo /usr/local/bin/railway-entrypoint.sh
+/usr/local/bin/railway-entrypoint.sh echo "setup"
 
 echo "-> Create empty common site config"
-echo "{}" > /home/frappe/bench/sites/common_site_config.json
+su frappe -c "echo '{}' > /home/frappe/bench/sites/common_site_config.json"
 
 echo "-> Create new site with ERPNext"
-bench new-site ${RFP_DOMAIN_NAME} --admin-password ${RFP_SITE_ADMIN_PASSWORD} --no-mariadb-socket --db-root-password ${RFP_DB_ROOT_PASSWORD} --install-app erpnext
-bench use ${RFP_DOMAIN_NAME}
+cd /home/frappe/bench
+su frappe -c "bench new-site ${RFP_DOMAIN_NAME} --admin-password ${RFP_SITE_ADMIN_PASSWORD} --no-mariadb-socket --db-root-password ${RFP_DB_ROOT_PASSWORD} --install-app erpnext"
+su frappe -c "bench use ${RFP_DOMAIN_NAME}"
 
 echo "-> Enable scheduler"
-bench enable-scheduler
+su frappe -c "bench enable-scheduler"
+
+echo "-> Setup complete! Remove the start command override, set port to 80, and redeploy."
